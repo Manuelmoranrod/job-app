4432b5ff6f192d135131ec320275d266a9b2be33
Christian <hiperboreos@gmail.com>
2021-09-03 13:06:00 +0200

    create route favorites and pug

--- routes/front-routes.js
+++ routes/front-routes.js
@@ -1,8 +1,22 @@
-const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
 
-const router = require('express').Router()
-const frontViews = require('../controllers/frontViews')
-//Front routes
-router.get('/', frontViews.home)
-router.get('/singup', frontViews.singup)//inscription formulary
-router.get('/login', frontViews.login)// login with auth
+
+router.get('/favorites', (req, res) => {
+    res.render('favoritos')
+});
+
+
+
+
+
+module.exports = router
+
+
+
+// const router = require('express').Router()
+// const frontViews = require('../controllers/frontViews')
+// //Front routes
+// router.get('/', frontViews.home)
+// router.get('/singup', frontViews.singup)//inscription formulary
+// router.get('/login', frontViews.login)// login with auth
